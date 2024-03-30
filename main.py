57b0fe6053dd96113cc1bce7e21f01613902d368
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-03-30 13:59:12 +0500

    configuration added

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import firebase_admin
 from firebase_admin import credentials, storage
 
 # Initialize Firebase
-cred = credentials.Certificate("....path/to/your/jsonfile")
-firebase_admin.initialize_app(cred, {'storageBucket': 'your-bucket-id'})
+cred = credentials.Certificate("cctv-security-system-firebase-adminsdk-h91tv-8bdd8dece7.json")
+firebase_admin.initialize_app(cred, {'storageBucket': 'gs://cctv-security-system.appspot.com'})
 
 # cctv-security-system-firebase-adminsdk-h91tv-8bdd8dece7.json
 # gs://cctv-security-system.appspot.com
-image = cv2.imread('pic.jpg')
+
+config ={ 
+    "apiKey": "AIzaSyAKxtVvABDyKusllWFQIQqRbKqkvkp063s",
+    "authDomain": "cctv-security-system.firebaseapp.com",
+    "projectId": "cctv-security-system",
+    "storageBucket": "cctv-security-system.appspot.com",
+    "messagingSenderId": "606255301184",
+    "appId": "1:606255301184:web:118b926d197b58feea95a5",
+    "measurementId": "G-9HH0NJY7N0",
+    "serviceAccount": "cctv-security-system-firebase-adminsdk-h91tv-8bdd8dece7.json"
+}
+image = cv2.imread('images\pic.jpg')
 # Function to upload image to Firebase storage
 def upload_to_firebase(image):
     if isinstance(image, np.ndarray):
